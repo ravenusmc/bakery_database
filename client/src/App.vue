4ceb118f03395b717e4e6c51be3bbfb08595bce0
ravenusmc <mcuddy77@gmail.com>
2023-08-16 16:54:50 -0400

    added navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <Navbar />
     <router-view/>
   </div>
 </template>
+
+<script>
+import Navbar from "@/components/common/Navbar.vue";
+// import Footer from "@/components/common/Footer.vue";
+
+export default {
+    components: {
+    Navbar,
+    // Footer,
+  },
+}
+</script>
+
+
 
 <style>
 </style>
