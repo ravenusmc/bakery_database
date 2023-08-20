932e8b2cf335ceca42d8c232085ab72c8f8bd331
ravenusmc <mcuddy77@gmail.com>
2023-08-20 13:50:20 -0400

    minor change

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <HelloWorld />
+    <Top />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Top from '@/components/landing/Top.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Top
   }
 }
 </script>
