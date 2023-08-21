63b9a3b3c2d345692445f22ebcdc13c98c1cd142
ravenusmc <mcuddy77@gmail.com>
2023-08-21 17:13:07 -0400

    working on landing page

--- client/src/App.vue
+++ client/src/App.vue
@@ -21,4 +21,8 @@ export default {
 
 
 <style>
+
+.center {
+  text-align: center;
+}
 </style>
--- client/src/components/landing/Top.vue
+++ client/src/components/landing/Top.vue
@@ -1,6 +1,12 @@
 <template>
 	<div>
-		<h1>test</h1>
+		<section>
+			<div class='pic-div'>
+			</div>
+			<div class="paragragh-div">
+ 				<h1 class="center heading">Bakery Database</h1>	
+			</div>
+		</section>
 	</div>
 </template>
 
@@ -10,3 +16,27 @@ export default {
 }
 
 </script>
+
+<style scoped>
+
+section {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  height: 600px;
+  margin: 50px 0 50px 0;
+}
+
+.pic-div {
+	display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: url("../../assets/images/cupcakes.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+.heading {
+  text-transform: uppercase;
+}
+
+</style>
