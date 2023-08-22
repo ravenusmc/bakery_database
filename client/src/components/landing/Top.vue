ec843cdabef50841d7fbb284c0ec0487e4ed5d2a
ravenusmc <mcuddy77@gmail.com>
2023-08-22 17:22:20 -0400

    added more to landing page

--- client/src/components/landing/Top.vue
+++ client/src/components/landing/Top.vue
@@ -5,6 +5,18 @@
 			</div>
 			<div class="paragragh-div">
  				<h1 class="center heading">Bakery Database</h1>	
+				<p>There's a bakery by my house that my family and I go to on a regular basis. 
+					I've often wondered how do they know how many items to put out? Why don't they
+					try new products? How many products do they role over to the next day? At times 
+					my wife and I will discuss these questions and I've told her for a while that I 
+					want to build a system that can help a bakery answer some of those questions.
+				</p>
+				<p>
+					This project is designed to be a system that a bakery could use to track the items
+					that it has available and to see which items are selling. If they have a certain type
+					of donut that is not selling, then why continue to sell that item? That's the main
+					purpose and goal of this project.
+				</p>
 			</div>
 		</section>
 	</div>
