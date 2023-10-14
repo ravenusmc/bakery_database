933d60d5246574779796261ce011c4ef554af972
ravenusmc <mcuddy77@gmail.com>
2023-10-14 09:03:09 -0400

    worked on mysql tables

--- server/sql/tables.sql
+++ server/sql/tables.sql
@@ -18,8 +18,40 @@ CREATE TABLE users
 CREATE TABLE goods
 (
   good_id INT NOT NULL AUTO_INCREMENT,
-  firstName VARCHAR(240) NOT NULL,
-  lastName VARCHAR(240) NOT NULL,
-
-  PRIMARY KEY(user_id)
+  good_name VARCHAR(240) NOT NULL,
+  amount INT NOT NULL,
+  date_stamp datetime NOT NULL,
+  PRIMARY KEY(good_id)
 );
+-- May need to add a datetime stamp to table
+
+
+Show databases;
+
+DESCRIBE Goods;
+
+DROP TABLE goods;
+
+--Reference stuff: 
+
+-- Inserting data 
+INSERT INTO goods
+(good_name, amount, date_stamp)
+VALUES ('Blueberry Muffin', 3, curdate());
+-- INSERT INTO actions
+-- VALUES (1, 1, True, 'TST-TST-2023-0001', 101, 'BWA', 'ie7046', '2023-01-01', False, True, True);
+
+DELETE FROM actions WHERE action_id = 1;
+
+SELECT COUNT(NOA) FROM actions WHERE Processor_ieNumber = 'ie7001' AND
+                NOA = 101;
+
+--Online references 
+Time Stamp information:
+https://stackoverflow.com/questions/37616760/how-to-insert-timestamp-into-my-mysql-table
+"INSERT INTO contactinfo 
+(name, email, subject, date, comments)
+VALUES 
+('$name', '$email', '$subject', NOW(), '$comments')"
+
+Another reference: 
